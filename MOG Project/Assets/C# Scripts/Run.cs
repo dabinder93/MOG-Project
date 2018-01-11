712605d90ef99b68072a5e56d0aff2b847ecd394
Klinar Christoph <christoph.klinar@solvistas.com>
2018-01-11 22:19:29 +0100

    char v1

---
+++ MOG Project/Assets/C# Scripts/Run.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Run : MonoBehaviour
+{
+
+    Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movemment();
+        {
+            float move = Input.GetAxis("Horizontal");
+            anim.SetFloat("Speed", move);
+        }
+    }
+
+
+    void Movemment()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * 3f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Vector2.right * 3f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+
+    }
+}
