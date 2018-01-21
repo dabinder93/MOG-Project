bcd8ec84991e523f1c4f38784f983deda404d3c4
dabinder93 <david.binder1993@gmail.com>
2018-01-21 17:10:57 +0100

    Added Checkpoint Torch Animation when hitting the Checkpoint

---
+++ MOG Project/Assets/C# Scripts/CheckpointManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour {
+
+    public LevelManager levelmanager;
+    private Animator anim;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        levelmanager = FindObjectOfType<LevelManager>();
+        anim = GetComponent<Animator>();
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            levelmanager.currentCheckpoint = gameObject;
+            anim.SetBool("isActivated", true);
+        }
+    }
+}
