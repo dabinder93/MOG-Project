27868958c034e7680c440d9f1a69b7d776ffc28a
Klinar Christoph <christoph.klinar@solvistas.com>
2018-01-31 00:59:52 +0100

    menu v2, healthbar v1

---
+++ MOG Project/Assets/C# Scripts/HealthLogic.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthLogic : MonoBehaviour {
+
+    public Sprite healthFull;
+    public Sprite healthEmpty;
+
+    float timer = 1f;
+    float delay = 1f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            if(this.gameObject.GetComponent<SpriteRenderer>().sprite == healthFull)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = healthEmpty;
+                timer = delay;
+                return;
+            }
+            if (this.gameObject.GetComponent<SpriteRenderer>().sprite == healthEmpty)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = healthFull;
+            }
+        }
+	}
+}
--- MOG Project/Assets/C# Scripts/KillPlayer.cs
+++ MOG Project/Assets/C# Scripts/KillPlayer.cs
@@ -23,4 +23,5 @@ public class KillPlayer : MonoBehaviour {
             levelmanager.RespawnPlayer();
         }
     }
+
 }
