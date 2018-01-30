1018460804f6a8bcbf323d026e5d85693bacba86
Klinar Christoph <christoph.klinar@solvistas.com>
2018-01-30 23:37:59 +0100

    menu v1

---
+++ MOG Project/Assets/MenuControll.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControll : MonoBehaviour {
+
+    public void changeMenuScene(string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+
+
+    public void closeGame()
+    {
+        Application.Quit();
+    }
+}
