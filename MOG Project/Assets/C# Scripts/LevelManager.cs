435e45a0dc720060d8188726e9695b573ef73690
dabinder93 <david.binder1993@gmail.com>
2018-01-31 22:45:42 +0100

    Added Finish Game Script

--- MOG Project/Assets/C# Scripts/LevelManager.cs
+++ MOG Project/Assets/C# Scripts/LevelManager.cs
@@ -6,6 +6,7 @@ public class LevelManager : MonoBehaviour {
 
     public GameObject currentCheckpoint;
     private PlayerController player;
+
 	// Use this for initialization
 	void Start () {
         player = FindObjectOfType<PlayerController>();
@@ -22,5 +23,10 @@ public class LevelManager : MonoBehaviour {
         //player.transform.position = currentCheckpoint.transform.position;
         player.transform.position = new Vector2(currentCheckpoint.transform.position.x, currentCheckpoint.transform.position.y + 0.6f);
 
+    }
+
+    public void FinishedGame()
+    {
+        Application.LoadLevel("Level/StartMenu");
     }
 }
